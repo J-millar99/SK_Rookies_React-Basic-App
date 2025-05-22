469094960484539fe0130c00910656fd43d4bbd4
J-millar99 <wlwogus33@gmail.com>
2025-05-22 23:48:48 +0900

    MyComponent useState

--- hello-react-app/src/components/MyComponentFunc.jsx
+++ hello-react-app/src/components/MyComponentFunc.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 const MyComponentFunc = ({ name, children }) => {
+    // 상태변수와 setter 함수 선언ㄴ하기
+    const {value, setValue} = useState(0);
+
     return (
         <div>
             <h2>함수형 컴포넌트</h2>
             <h3>Hello! {name} </h3>
             {children}
+            <p>상태변수 value={value} </p>
+            <button onClick={()=>(setValue(value + 1))}>증가</button>
+            <button onClick={()=>(setValue(value - 1))}>감소</button>
         </div>
     );
 };
