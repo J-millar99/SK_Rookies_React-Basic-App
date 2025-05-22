429871ac67dfc59e88781f036ca6d2dae9dafc77
J-millar99 <wlwogus33@gmail.com>
2025-05-22 21:41:49 +0900

    MyComponent proptypes

---
+++ hello-react-app/src/components/MyComponent.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello! {this.props.name}</h3>
+      </div>
+    )
+  }
+} // class
+
+MyComponent.defaultProps = {
+    name: '리액트JS'
+};
+
+MyComponent.propTypes = {
+    name: PropTypes.string
+};
