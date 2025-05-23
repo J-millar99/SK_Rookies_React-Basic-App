3dbaf525ddc71b8c4af7dd0f8a1008ef22faa61b
J-millar99 <wlwogus33@gmail.com>
2025-05-23 19:19:54 +0900

    Component

--- hello-react-app/src/components/MyComponent.jsx
+++ hello-react-app/src/components/MyComponent.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react'
 import PropTypes from 'prop-types'
+import './MyComponent.css';
 
 export default class MyComponent extends Component {
   // 상태변수를 포함하는 state 객체 선언
@@ -7,6 +8,7 @@ export default class MyComponent extends Component {
     value: 0,
     message: '',
     username: '',
+    isValid: false,
   }
 
   handleChange = (e) => {
@@ -22,8 +24,8 @@ export default class MyComponent extends Component {
     }));
   render() {
     const {name, age} = this.props;
-    const { value, message, username } = this.state;
-    const {handleDecrement, handleChange } = this;
+    const { value, message, username, isValid } = this.state;
+    const { handleDecrement, handleChange } = this;
     return (
       <div>
         <h2>클래스형 컴포넌트</h2>
@@ -36,7 +38,10 @@ export default class MyComponent extends Component {
         <p>State message의 값 = {message}</p>
         <input name="message" value={message} onChange={handleChange} /> <br/>
         <p>State username의 값 = {username}</p>
-        <input name="username" value={username} onChange={handleChange} /> <br/>
+        <input name="username" value={username} onChange={handleChange} 
+          className={ isValid ? 'success' : 'failure' }
+          ref={(ref) => this.myUsername=ref}
+        /> <br/>
       </div>
     )
   }
--- hello-react-app/src/components/MyComponentFunc.jsx
+++ hello-react-app/src/components/MyComponentFunc.jsx
@@ -3,6 +3,20 @@ import { useState } from "react";
 const MyComponentFunc = ({ name, children }) => {
     // 상태변수와 setter 함수 선언하기
     const [value, setValue] = useState(0);
+    // const [message, setMessage] = useState('');
+    // const [username, setUsername] = useState('');
+    const [inputs, setInputs] = useState({
+        message: '', username: ''
+    })
+
+    const handleChange = (e) => {
+        setInputs({
+            ...inputs,
+            [e.target.name]: e.target.value
+        })
+    };
+
+    const { message, username } = inputs;
 
     return (
         <div>
@@ -12,6 +26,10 @@ const MyComponentFunc = ({ name, children }) => {
             <p>상태변수 value={value} </p>
             <button onClick={()=>(setValue(value + 1))}>증가</button>
             <button onClick={()=>(setValue(value - 1))}>감소</button>
+            <p>State message의 값 = {message}</p>
+            <input name="message" value={message} onChange={handleChange} /> <br/>
+            <p>State username의 값 = {username}</p>
+            <input name="username" value={username} onChange={handleChange} /> <br/>
         </div>
     );
 };
